eea8c31a759e2db9a49092de9e6ec409db1fad36
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:22:25 +0300

    Добавил метод MultiplicationAdder который прошёл базовый тест

---
+++ StringCalculator/MultiplicationAdder.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class MultiplicationAdder
+    {
+        public string AddMultiplicationSign(string expression)
+        {
+            return Regex.Replace(expression, @"(\d)(\()", "$1*$2")
+                         .Replace(@")(\d)", ")*$1");
+        }
+    }
+}
