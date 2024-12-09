a0b2600db13bd9371ab1bcafa034c7f3e2aa91f6
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:06:52 +0300

    Написал метод по валидации ввода, протестировал на базовых тестах

---
+++ StringCalculator/ExpressionValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class ExpressionValidator
+    {
+        public void Validate(string expression)
+        {
+
+            if (!Regex.IsMatch(expression, @"^[0-9\+\-\*/\(\)\.]+$"))
+            {
+                throw new FormatException("Выражение содержит недопустимые символы.");
+            }
+
+            int openParenthesesCount = 0;
+            foreach (char c in expression)
+            {
+                if (c == '(') openParenthesesCount++;
+                if (c == ')') openParenthesesCount--;
+            }
+
+            if (openParenthesesCount != 0)
+            {
+                throw new FormatException("Несовпадающие круглые скобки.");
+            }
+
+            if (Regex.IsMatch(expression, @"[\+\-\*/]{2,}"))
+            {
+                throw new FormatException("Выражение содержит последовательные операторы.");
+            }
+        }
+    }
+}
