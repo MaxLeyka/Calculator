5098354fbe10de5649312b571d551ddb4d00cd99
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:02:44 +0300

    Написал второй тест на базовую проверку работы метода проверяющим валидность ввода, а также дополнил первый тест большим кол-вом тестов

---
+++ StringCalculator.Tests/ExpressionValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class ExpressionValidatorTests
+    {
+        private readonly ExpressionValidator _expressionValidator = new ExpressionValidator();
+
+        [Fact]
+        public void Test_ValidExpression()
+        {
+            var expression = "2+3*(4-1)";
+            _expressionValidator.Validate(expression);
+        }
+
+        [Fact]
+        public void Test_InvalidExpression()
+        {
+            var expression = "2++2";
+            Assert.Throws<FormatException>(() => _expressionValidator.Validate(expression));
+        }
+    }
+}
