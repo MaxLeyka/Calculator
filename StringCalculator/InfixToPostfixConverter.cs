07beb8eab1b8c6b5247fa8539f7c7319a41cdf26
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 22:09:22 +0300

    Произведён рефакторинг изменённого класса ExpressionEvaluator с помощью разделения логики на два отдельных класса и использования изначального класса как посредника, все тесты проходятся

---
+++ StringCalculator/InfixToPostfixConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class InfixToPostfixConverter
+    {
+        public Queue<string> Convert(string expression)
+        {
+            var output = new Queue<string>();
+            var operators = new Stack<string>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+
+                if (char.IsDigit(c) || c == '.')
+                {
+                    output.Enqueue(ReadNumber(expression, ref i));
+                }
+                else if (c == '(')
+                {
+                    operators.Push("(");
+                }
+                else if (c == ')')
+                {
+                    while (operators.Peek() != "(")
+                        output.Enqueue(operators.Pop());
+                    operators.Pop(); // Удаляем '('
+                }
+                else if (IsOperator(c))
+                {
+                    if (IsUnaryMinus(expression, i))
+                        operators.Push("u-");
+                    else
+                        ProcessOperator(c.ToString(), operators, output);
+                }
+            }
+
+            while (operators.Count > 0)
+                output.Enqueue(operators.Pop());
+
+            return output;
+        }
+
+        private void ProcessOperator(string op, Stack<string> operators, Queue<string> output)
+        {
+            while (operators.Count > 0 && operators.Peek() != "(" && Precedence(op) <= Precedence(operators.Peek()))
+                output.Enqueue(operators.Pop());
+            operators.Push(op);
+        }
+
+        private string ReadNumber(string expression, ref int index)
+        {
+            int start = index;
+            while (index < expression.Length && (char.IsDigit(expression[index]) || expression[index] == '.'))
+                index++;
+            index--; // Откат на последний символ числа
+            return expression.Substring(start, index - start + 1);
+        }
+
+        private bool IsOperator(char c) => "+-*/".Contains(c);
+
+        private int Precedence(string op) =>
+            op switch
+            {
+                "+" or "-" => 1,
+                "*" or "/" => 2,
+                "u-" => 3,
+                _ => throw new InvalidOperationException($"Неизвестный оператор: {op}")
+            };
+
+        private bool IsUnaryMinus(string expression, int index) =>
+            expression[index] == '-' && (index == 0 || "()+-*/".Contains(expression[index - 1]));
+    }
+}
---
+++ StringCalculator/PostfixEvaluator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class PostfixEvaluator
+    {
+        public double Evaluate(Queue<string> postfix)
+        {
+            var stack = new Stack<double>();
+
+            while (postfix.Count > 0)
+            {
+                string token = postfix.Dequeue();
+                if (double.TryParse(token, out double number))
+                    stack.Push(number);
+                else
+                    ProcessOperator(token, stack);
+            }
+
+            return stack.Pop();
+        }
+
+        private void ProcessOperator(string token, Stack<double> stack)
+        {
+            if (token == "u-")
+            {
+                stack.Push(-stack.Pop());
+            }
+            else
+            {
+                double b = stack.Pop();
+                double a = stack.Pop();
+                stack.Push(ApplyOperator(token[0], a, b));
+            }
+        }
+
+        private double ApplyOperator(char op, double a, double b) =>
+            op switch
+            {
+                '+' => a + b,
+                '-' => a - b,
+                '*' => a * b,
+                '/' => a / b,
+                _ => throw new InvalidOperationException($"Недопустимый оператор: {op}")
+            };
+    }
+}
