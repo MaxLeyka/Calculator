bcc84e89bc194d5f2b50a410877088e31e0a4095
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 17:46:05 +0300

    Реализация метода вычисления математических выражений (проверка теста успешно пройдена)

---
+++ StringCalculator/ExpressionEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace StringCalculator
+{
+    public class ExpressionEvaluator
+    {
+        public double Evaluate(string expression)
+        {
+            var dataTable = new DataTable();
+            try
+            {
+                return Convert.ToDouble(dataTable.Compute(expression, string.Empty));
+            }
+            catch (Exception ex)
+            {
+                throw new FormatException("Недопустимое выражение", ex);
+            }
+        }
+    }
+}
