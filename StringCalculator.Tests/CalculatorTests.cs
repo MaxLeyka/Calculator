7e7e8c0411214c1c3f072043717568fc64841ea5
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:32:28 +0300

    Написал базовый тест для проверки работы всего калькулятора

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Test_Calculate_ValidExpression_Addition()
+        {
+            var expression = "2 + 2";
+            var result = Calculator.Calculate(expression);
+            Assert.Equal(4, result);
+        }
+    }
+}
