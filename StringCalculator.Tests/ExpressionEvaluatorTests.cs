ba59ebc08fa36e8e8b739dc9a2bc60c7fa0660ae
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 17:42:17 +0300

    Создание базового теста для метода по вычислению матемачиеских выражений

---
+++ StringCalculator.Tests/ExpressionEvaluatorTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class ExpressionEvaluatorTests
+    {
+        private readonly ExpressionEvaluator _expressionEvaluator = new ExpressionEvaluator();
+
+        [Fact]
+        public void Test_Evaluate_Addition()
+        {
+            var expression = "2 + 2";
+            var result = _expressionEvaluator.Evaluate(expression);
+            Assert.Equal(4, result);
+        }
+    }
+}
