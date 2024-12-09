3fce1cb891275cb1f2c473c938572b327e61ee04
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:19:20 +0300

    "Добавлен базовый тест для метода MultiplicationAdder (проверка добавления знака умножения между числом и скобками)

---
+++ StringCalculator.Tests/MultiplicationAdderTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class MultiplicationAdderTests
+    {
+        private readonly MultiplicationAdder _multiplicationAdder = new MultiplicationAdder();
+
+        [Fact]
+        public void Test_AddMultiplicationSign()
+        {
+            var result = _multiplicationAdder.AddMultiplicationSign("2(3+5)");
+            Assert.Equal("2*(3+5)", result);
+        }
+    }
+}
