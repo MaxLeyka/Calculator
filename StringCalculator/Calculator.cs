a254ae731b0d3906923f6393e34708f164745feb
MaxLeyka <158818982+MaxLeyka@users.noreply.github.com>
2024-12-09 18:36:02 +0300

    Добавил основной класс Calculatator с методом Calculate и протестировал его

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace StringCalculator
+{
+    public static class Calculator
+    {
+        private static readonly MultiplicationAdder _multiplicationAdder = new MultiplicationAdder();
+        private static readonly ExpressionValidator _expressionValidator = new ExpressionValidator();
+        private static readonly ExpressionEvaluator _expressionEvaluator = new ExpressionEvaluator();
+
+        public static double Calculate(string expression)
+        {
+            string cleanedExpression = expression.Replace(" ", "");
+
+            cleanedExpression = _multiplicationAdder.AddMultiplicationSign(cleanedExpression);
+
+            _expressionValidator.Validate(cleanedExpression);
+
+            return _expressionEvaluator.Evaluate(cleanedExpression);
+        }
+    }
+}
